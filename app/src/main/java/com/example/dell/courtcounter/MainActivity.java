581c8819820ec67dadd116ff1ae404aa589951a6
FLYINGKRIPTO <srivastava.shivanshu4@gmail.com>
2018-06-07 12:57:52 +0530

    Applied dimens.xml for dimensions Implemented ScrollView for Screen Rotation

--- app/src/main/java/com/example/dell/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/dell/courtcounter/MainActivity.java
@@ -1,64 +1,75 @@
 package com.example.dell.courtcounter;
-
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
-
 public class MainActivity extends AppCompatActivity {
     int scoreTeamA = 0;
     int scoreTeamB = 0;
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater= getMenuInflater();
+        inflater.inflate(R.menu.activity_main,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.reset_scores:
+                resetScores();
+                return true;
 
-
+            default:
+                return super.onCreateOptionsMenu((Menu) item);
+        }
+    }
+    private void resetScores() {
+        scoreTeamA = 0;
+        displayForTeamA(scoreTeamA);
+        scoreTeamB = 0;
+        displayForTeamB(scoreTeamB);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
     public void addThreeForTeamA(View v) {
         scoreTeamA = scoreTeamA + 3;
         displayForTeamA(scoreTeamA);
     }
-
     public void addTwoForTeamA(View v) {
         scoreTeamA = scoreTeamA + 2;
         displayForTeamA(scoreTeamA);
     }
-
     public void addOneForTeamA(View v) {
         scoreTeamA = scoreTeamA + 1;
         displayForTeamA(scoreTeamA);
     }
-
     public void addThreeForTeamB(View v) {
         scoreTeamB = scoreTeamB + 3;
         displayForTeamB(scoreTeamB);
     }
-
     public void addTwoForTeamB(View v) {
         scoreTeamB = scoreTeamB + 2;
         displayForTeamB(scoreTeamB);
     }
-
     public void addOneForTeamB(View v) {
         scoreTeamB = scoreTeamB + 1;
         displayForTeamB(scoreTeamB);
     }
-
-
     public void displayForTeamA(int score) { TextView team_a_score = findViewById(R.id.team_a_score);
-        team_a_score.setText(String.valueOf(score));}
-
-
+        team_a_score.setText(String.valueOf(score));
+    }
     public void displayForTeamB(int score) { TextView team_b_score = findViewById(R.id.team_b_score);
-        team_b_score.setText(String.valueOf(score));}
-
+        team_b_score.setText(String.valueOf(score));
+    }
     public void resetButton(View v) {
         scoreTeamA = 0;
         displayForTeamA(scoreTeamA);
         scoreTeamB = 0;
         displayForTeamB(scoreTeamB);
     }
-
 }
